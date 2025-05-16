586245210b22c1c9608edc4e39b05520dd98680c
Eduardo Sanches <edupsanches@gmail.com>
2025-05-16 00:30:15 -0300

    more API studies using json-server

--- 7-working-apis/1-installing-json-server/main.js
+++ 7-working-apis/1-installing-json-server/main.js
@@ -1,3 +1,11 @@
+// const response = fetch("http://localhost:3000")
+
+// console.log(response)
+
+fetch("http://localhost:3000/products")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+
 /*
     Documentação para:
         • Levantar o container docker: "docker-compose up -d"
@@ -9,4 +17,13 @@
 
         No arquivo package.json é possível mudar a porta usando a flag --port=3333
         (ao usar esse recurso, não esquecer de mudar a porta também no arquivo docker-compose.yml, se deixar sem nada na flag a porta usada será a determinada no docker-compose.yml)
+*/
+
+/*
+    TROCAR A VERSÃO
+    • Para garantir que as funcionalidades do pacote "json-server" funcionem exatamente como demonstrado no curso é necessário usar a mesma versão do professor.
+    Para trocar a versão é só entrar no npmjs.com ver as versões e instalar dessa forma:
+
+    npm install json-server@1.0.0-alpha.17
+    (versão 1.0.0-alpha.17 é apenas um exemplo de versão)
 */
