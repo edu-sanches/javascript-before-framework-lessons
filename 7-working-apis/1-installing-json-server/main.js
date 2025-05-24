2fc781b036802dd41430a867d25d18e671312684
Eduardo Sanches <edupsanches@gmail.com>
2025-05-24 18:39:19 -0300

    tests on API

--- 7-working-apis/1-installing-json-server/main.js
+++ 7-working-apis/1-installing-json-server/main.js
@@ -9,10 +9,39 @@
 //     .then((data) => console.log(data))
 
 // Utilizando o fetch com async/await
-async function fetchFunction() {
-    const response = await fetch("http://localhost:3000/products")
+async function fetchProducts() {
+    const response = await fetch("http://localhost:3333/products")
     const data = await response.json()
     console.log(data)
 }
 
-fetchFunction()
+// fetchProducts()
+
+
+// Passando parâmetros na requisição
+async function fetchProductsByEndpointId(endpoint, id) {
+    const response = await fetch(`http://localhost:3333/${endpoint}/${id}`)
+    const data = await response.json()
+    console.log(data)
+}
+
+// fetchProductsByEndpointId('products', '2')
+
+const productName = document.getElementById("name")
+const productPrice = document.getElementById("price")
+
+addEventListener("submit", async (event) => {
+    event.preventDefault()
+
+    await fetch("http://localhost:3333/products", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: new Date().getTime().toString(),
+            name: productName.value,
+            price: productPrice.value
+        })
+    })
+})
